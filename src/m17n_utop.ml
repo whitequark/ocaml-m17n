d422ea63a9beaaed93468008cc0f8aa7f84c9784
Peter Zotov <whitequark@whitequark.org>
2014-12-11 23:28:26 +0300

    M17n_wrap → M17n_util.

--- src/m17n_utop.ml
+++ src/m17n_utop.ml
@@ -4,12 +4,12 @@ let () =
     print_endline "m17n is incompatible with camlp4!"
   else begin
     UTop.parse_toplevel_phrase :=
-      UTop.parse_default (M17n_wrap.internationalize Parser.toplevel_phrase);
+      UTop.parse_default (M17n_util.internationalize Parser.toplevel_phrase);
     UTop.parse_use_file :=
-      UTop.parse_default (M17n_wrap.internationalize Parser.use_file);
-    Toploop.print_out_sig_item := M17n_wrap.utf8_print_out_sig_item !Toploop.print_out_sig_item;
+      UTop.parse_default (M17n_util.internationalize Parser.use_file);
+    Toploop.print_out_sig_item := M17n_util.utf8_print_out_sig_item !Toploop.print_out_sig_item;
     Toploop.install_printer Predef.path_string Predef.type_string
-      (fun fmt obj -> M17n_wrap.utf8_print_string fmt (Obj.magic obj));
+      (fun fmt obj -> M17n_util.utf8_print_string fmt (Obj.magic obj));
     (* As of 1.16, utop does not actually invoke UTop.parse_use_file. *)
-    Toploop.parse_use_file := M17n_wrap.internationalize Parser.use_file;
+    Toploop.parse_use_file := M17n_util.internationalize Parser.use_file;
   end
